527875cbc30fa262dafa4751cf1c8bea6b9d38d4
Nguyenthikhanhhong25174600219KHDL19A4HN <nguyenthikhanhhong06ss@gmail.com>
2026-04-08 10:24:41 +0700

    Delete 5.1_25174600219_NguyenThiKhanhHong/Bài 9.py

--- 5.1_25174600219_NguyenThiKhanhHong/Bài 9.py
+++
@@ -1,9 +0,0 @@
-m = int(input("Nhập m: "))
-n = int(input("Nhập n: "))
-x = tuple(range(m, n + 1))
-y = len(x) // 2
-x1 = x[:y]
-x2 = x[y:]
-print("Tuple ban đầu:", x)
-print("Nửa đầu (tp1):", x1)
-print("Nửa cuối (tp2):", x2)
